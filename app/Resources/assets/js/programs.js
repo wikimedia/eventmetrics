229533ea298b2f4c121c5442829956a074abadd1
MusikAnimal <musikanimal@gmail.com>
2017-11-02 02:03:23 -0400

    Programs index and new pages, associated JS
    Tweaks to layout, link to OAuth on splash page

--- app/Resources/assets/js/programs.js
+++ app/Resources/assets/js/programs.js
@@ -6,18 +6,30 @@ $(function () {
 
 function setupOrganizerForm()
 {
+    // keep track of how many email fields have been rendered
+    var organizerCount = $('.program__organizers .organizer-row').length;
+
     $('.add-organizer').on('click', function (e) {
         e.preventDefault();
 
+        // Clone the row and correct CSS classes.
         var $orgRow = $('.organizer-row__template')
             .clone()
             .removeClass('hidden')
             .removeClass('organizer-row__template');
 
-        // $orgRow.insertAfter('.organizer-row:last');
+        // Insert after the last row.
         $('.organizer-row:last').after($orgRow);
 
         var $newRow = $('.organizer-row:last');
+
+        // Add name attribute to the input of the new row.
+        $newRow.find('input').prop('name', 'form[organizerNames][' + organizerCount + ']');
+
+        // Increment count so the next added row will have the correct name attribute.
+        organizerCount++;
+
+        // Add listener to remove the row.
         $newRow.find('.remove-organizer').on('click', function () {
             $newRow.remove();
         });
