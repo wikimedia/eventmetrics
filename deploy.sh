5747f811ff879e38e1a58cd1daf254d574cf1e11
MusikAnimal <musikanimal@gmail.com>
2018-07-18 16:05:17 -0400

    Allow specifiying branch name in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
-git pull
+#!/bin/bash
+if [ ! -z "$1" ]; then
+  git fetch origin
+  git checkout -t origin/$1
+  git checkout $1
+  git pull origin $1
+else
+  git checkout master
+  git pull origin master
+fi
 composer install
 php bin/console cache:clear --env=prod --no-warmup
 php bin/console assetic:dump --env=prod
