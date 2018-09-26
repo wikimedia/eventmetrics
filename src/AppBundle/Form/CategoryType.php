3120e0c18aab1f9d146c27409cf17048a22360f2
MusikAnimal <musikanimal@gmail.com>
2018-09-26 13:25:37 -0400

    Form classes, Twig template, and i18n messages for category frontend
    This was reviewed as part of #115. Merging as a separate commit
    to make further review easier.
    
    Bug: https://phabricator.wikimedia.org/T194703

---
+++ src/AppBundle/Form/CategoryType.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * This file contains only the CategoryType class.
+ */
+
+declare(strict_types=1);
+
+namespace AppBundle\Form;
+
+use AppBundle\Model\Event;
+use AppBundle\Model\EventCategory;
+use AppBundle\Repository\EventCategoryRepository;
+use AppBundle\Repository\EventWikiRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Valid;
+
+/**
+ * A CategoryType represents a single category row in the form.
+ */
+class CategoryType extends AbstractType
+{
+    /** @var Event */
+    private $event;
+
+    /** @var EventWikiRepository */
+    private $ewRepo;
+
+    /**
+     * CategoryType constructor.
+     * @param EventWikiRepository $ewRepo
+     * @param EventCategoryRepository $ecRepo
+     */
+    public function __construct(EventWikiRepository $ewRepo)
+    {
+        $this->ewRepo = $ewRepo;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $this->event = $options['event'];
+
+        $builder->add('domain', TextType::class, [
+                'constraints' => [new Valid()],
+                'empty_data' => '',
+            ])->add('title', TextType::class, [
+                'constraints' => [new Valid()],
+                'empty_data' => '',
+            ]);
+
+        $builder->get('domain')
+            ->addModelTransformer($this->getWikiCallbackTransformer());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => EventCategory::class,
+            'event' => null,
+            'required' => false,
+            'empty_data' => function (FormInterface $form) {
+                return new EventCategory(
+                    $this->event,
+                    $form->get('title')->getData(),
+                    $form->get('domain')->getData()
+                );
+            },
+            'error_mapping' => [
+                'categoryId' => 'title',
+            ],
+        ]);
+    }
+
+    /**
+     * Transform wiki data to or from the form.
+     * @return CallbackTransformer
+     */
+    private function getWikiCallbackTransformer(): CallbackTransformer
+    {
+        return new CallbackTransformer(
+            // Transform to the form.
+            function ($domain) {
+                // No transformation needed.
+                return $domain;
+            },
+            // Transform from the form.
+            function ($domain) {
+                // Turns 'en.wikipedia.org', 'enwiki', etc., into 'en.wikipedia'.
+                return (string)$this->ewRepo->getDomainFromEventWikiInput($domain);
+            }
+        );
+    }
+}
